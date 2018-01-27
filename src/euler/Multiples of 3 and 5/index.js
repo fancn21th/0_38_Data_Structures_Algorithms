c96dd18a98c88080ae21dc8e60695206715fd62e
fancn <fanyj@yikeer.com>
2018-01-27 16:02:46 +0800

    Multiples of 3 and 5 step 1. env setup step 2. two solutions

--- src/euler/Multiples of 3 and 5/index.js
+++ src/euler/Multiples of 3 and 5/index.js
@@ -1,5 +1,30 @@
+const number_one = 3,
+      number_two = 5;
+
+// const Multiples = (range) => {
+//   let i = number_one, sum = 0;
+//   while(i < range) {
+//     if (i % number_one === 0 || i % number_two === 0) {
+//       sum += i;
+//     }
+//     i++;
+//   }
+//   return sum;
+// };
+
+const get_multiple_of_number= (range, number) => {
+  let next = number, sum = 0, step = 1;
+  while(next < range){
+    sum += next;
+    next = number * ++step;
+  }
+  return sum;
+};
+
 const Multiples = (range) => {
-  return 23;
+  return get_multiple_of_number(range, number_one) +
+          get_multiple_of_number(range, number_two) -
+          get_multiple_of_number(range, number_one * number_two);
 };
 
 export default Multiples;
--- src/euler/Multiples of 3 and 5/index.test.js
+++ src/euler/Multiples of 3 and 5/index.test.js
@@ -7,3 +7,10 @@ test('The natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 
   timer.stop('multiples_10');
   console.log(timer.getFormattedTime('multiples_10'));
 });
+
+test('The natural numbers below 1000 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.', () => {
+  timer.start('multiples_1000');
+  expect(multiples(1000)).toBe(233168);
+  timer.stop('multiples_1000');
+  console.log(timer.getFormattedTime('multiples_1000'));
+});
