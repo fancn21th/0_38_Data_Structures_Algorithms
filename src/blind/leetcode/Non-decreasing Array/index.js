6a8e35c49569b4485eebb5453dc66d0b896bf0d7
alex.fan <alex.fan@okchem.com>
2018-09-20 23:57:32 +0800

    bad performance one

--- src/blind/leetcode/Non-decreasing Array/index.js
+++ src/blind/leetcode/Non-decreasing Array/index.js
@@ -1,10 +1,40 @@
 // Given an array with n integers, your task is to check if it could become non-decreasing by modifying at most 1 element.
 //
 // We define an array is non-decreasing if array[i] <= array[i + 1] holds for every i (1 <= i < n).
-/**
- * @param {number[]} nums
- * @return {boolean}
- */
+
+// // brute force way, bad performance
+// var checkPossibility = function(nums) {
+//   let count = 0,
+//       i = 0,
+//       n = 10000
+//   // sort and check if two cases break the expected sort results
+//   // iterate all n numbers
+//   for (;i<n;i++) {
+//     let j = i + 1
+//     // find the smallest
+//     for (;j<n;j++) {
+//       if(nums[j] < nums[i]) {
+//         count += 1
+//         if (count > 1) {
+//           return false
+//         }
+//         break
+//       }
+//     }
+//   }
+//   return true
+// };
+
+// what is the smallest unit to check anyway ?
 var checkPossibility = function(nums) {
+  // if a < b < c
+
+  // if a < b > c
+
+  // if a > b < c
+
+  // if a > b > c
 
 };
+
+export default checkPossibility
--- src/blind/leetcode/Non-decreasing Array/index.test.js
+++ src/blind/leetcode/Non-decreasing Array/index.test.js
@@ -1,5 +1,49 @@
 import checkPossibility from './index'
 
 test('[4,2,3]', () => {
-
+  const numbers = [4,2,3]
+  const result = checkPossibility(numbers)
+  expect(result).toEqual(true)
 })
+
+test('[4,2,1]', () => {
+  const numbers = [4,2,1]
+  const result = checkPossibility(numbers)
+  expect(result).toEqual(false)
+})
+
+test('[3,4,2,3]', () => {
+  const numbers = [3,4,2,3]
+  const result = checkPossibility(numbers)
+  expect(result).toEqual(false)
+})
+
+test('[3,3,4,5]', () => {
+  const numbers = [3,3,4,5]
+  const result = checkPossibility(numbers)
+  expect(result).toEqual(true)
+})
+
+test('[1 .. 10000] with 1 bad number', () => {
+  const numbers = []
+  var i = 1
+  for(;i<=10000;i++) {
+    numbers.push(i)
+  }
+  numbers[numbers.length - 2] = 10001
+  const result = checkPossibility(numbers)
+  expect(result).toEqual(true)
+})
+
+test('[1 .. 10000] with 2 bad numbers', () => {
+  const numbers = []
+  var i = 1
+  for(;i<=10000;i++) {
+    numbers.push(i)
+  }
+  numbers[numbers.length - 2] = 10001
+  numbers[numbers.length - 3] = 10002
+  const result = checkPossibility(numbers)
+  expect(result).toEqual(false)
+})
+
