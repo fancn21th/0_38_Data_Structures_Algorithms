934dc136202828f7165822c4d7afcca9540268b1
fancn <fanyj@yikeer.com>
2019-06-03 22:47:24 +0800

    update function of isBalanced

--- src/blind/data structure/balanced-parentheses.js
+++ src/blind/data structure/balanced-parentheses.js
@@ -1,5 +1,7 @@
 const isBalanced = function(expression) {
   const arr = expression.split("");
+  const stack = [];
+  arr.forEach(function(item) {});
   return true;
 };
 
--- src/blind/data structure/balanced-parentheses.test.js
+++ src/blind/data structure/balanced-parentheses.test.js
@@ -5,3 +5,9 @@ test("[]", function() {
   const result = isBalanced(testStr);
   expect(result).toEqual(true);
 });
+
+test("{}", function() {
+  const testStr = "{}";
+  const result = isBalanced(testStr);
+  expect(result).toEqual(true);
+});
