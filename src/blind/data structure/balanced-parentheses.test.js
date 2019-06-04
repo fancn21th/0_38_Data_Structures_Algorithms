b376ddb4465c3154d7ab9bcf501281d6159ef004
alex.fan <alex.fan@okchem.com>
2019-06-04 11:50:03 +0800

    more test cases for balanced parentheses

--- src/blind/data structure/balanced-parentheses.test.js
+++ src/blind/data structure/balanced-parentheses.test.js
@@ -1,14 +1,28 @@
 import isBalanced from "./balanced-parentheses";
 
 describe("balanced parrentheses testing", function() {
-  test("[]", function() {
-    const testStr = "[]";
+  test("{}()[{}]", function() {
+    const testStr = "{}()[{}]";
     const result = isBalanced(testStr);
     expect(result).toEqual(true);
   });
 
-  test("[{]", function() {
-    const testStr = "[{]";
+  test("({[]})", function() {
+    const testStr = "({[]})";
+    const result = isBalanced(testStr);
+    expect(result).toEqual(true);
+  });
+});
+
+describe("unbalanced parrentheses testing", function() {
+  test("[({)}]", function() {
+    const testStr = "[({)}]";
+    const result = isBalanced(testStr);
+    expect(result).toEqual(false);
+  });
+
+  test("()}[]", function() {
+    const testStr = "()}[]";
     const result = isBalanced(testStr);
     expect(result).toEqual(false);
   });
