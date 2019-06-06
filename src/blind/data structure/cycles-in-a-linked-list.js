10f1c31f145cde18a0374b24a33d67f52e23501b
alex.fan <alex.fan@okchem.com>
2019-06-06 18:21:56 +0800

    Node Class

---
+++ src/blind/data structure/cycles-in-a-linked-list.js
@@ -0,0 +1,12 @@
+class Node {
+  constructor(data) {
+    this._data = data;
+    this._next = null;
+  }
+
+  set next(next) {
+    this._next = next;
+  }
+}
+
+const hasCycle = function(headNode) {};
